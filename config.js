cb8b57db5ccae599113eb7d4dc3042e0b7047320
Ronny Eisenkolb <eisenkolb.ronny@gmail.com>
2015-09-14 15:27:57 +0200

    Updated configuration for eintopf proxy

---
+++ config.js
@@ -0,0 +1,57 @@
+// # Ghost Configuration
+// Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
+
+// Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
+
+var path = require('path'),
+    config;
+
+config = {
+
+    // ### Development **(default)**
+    development: {
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        // Change this to your Ghost blog's published URL.
+        url: 'http://ghost.dev:4480',
+
+        // Example mail config
+        // Visit http://support.ghost.org/mail for instructions
+        // ```
+        //  mail: {
+        //      transport: 'SMTP',
+        //      options: {
+        //          service: 'Mailgun',
+        //          auth: {
+        //              user: '', // mailgun username
+        //              pass: ''  // mailgun password
+        //          }
+        //      }
+        //  },
+        // ```
+
+        // #### Database
+        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        // #### Server
+        // Can be host & port (default), or socket
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '0.0.0.0',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        },
+        // #### Paths
+        // Specify where your content directory lives
+        paths: {
+            contentPath: path.join(process.env.GHOST_CONTENT, '/')
+        }
+    }
+};
+
+module.exports = config;
